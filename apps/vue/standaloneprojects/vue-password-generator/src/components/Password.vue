9d467d5292ce83f361c4c9297957c67308c72a99
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-10-24 12:48:06 +0100

    add examples for expressjs with typescript by express-typescript-generator and add a first vue3 standalone project based on existing github projects.

---
+++ apps/vue/standaloneprojects/vue-password-generator/src/components/Password.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {computed, onMounted, onUnmounted, ref, reactive, nextTick, watch, toRef, toRefs, useSlots} from 'vue';
+import get from "@/functions/get";
+const props = defineProps(["length", "special", "upper", "lower", "number"]);
+
+const password = computed(() =>
+    get(props.length, props.lower, props.upper, props.number, props.special)
+);
+
+const copied = ref(false);
+
+const copy = () => {
+  navigator.clipboard.writeText(password.value);
+  copied.value = true;
+};
+
+</script>
+
+<template>
+  <div class="mb-2 mt-8 p-2 text-center">
+    <div @click="copy" class="cursor-pointer font-bold text-xl">
+      {{ copied ? "Copied!" : password }}
+    </div>
+    <p class="text-xs text-gray-600 italic">click to copy</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
